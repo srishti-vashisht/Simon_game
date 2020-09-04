a56b87c4ff5db53aadbab56792a4fef22aef99e4
Srishti <srishti.vashisht@outlook.com>
2020-09-04 23:37:03 +0530

    First

---
+++ game.js
@@ -0,0 +1,15 @@
+var gamePattern=[];
+
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChoosenColour 
+
+function nextSequence()
+{
+    var RandomNumber= Math.floor(Math.random()*4);   //creating random no's b/w 0-3
+}
+
+var randomChoosenColour = buttonColours[RandomNumber];   //using randomNumber to select a random colour from the buttonColours array
+
+gamePattern.push(randomChoosenColour);
